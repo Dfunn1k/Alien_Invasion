8ece76f783b790030bfd72576f03f37bbe941804
Dfunn1k <danychavezdev@gmail.com>
2023-10-03 17:32:49 -0500

    feat: set fill color

--- alien_invasion.py
+++ alien_invasion.py
@@ -11,6 +11,7 @@ class AlienInvasion:
 
         self.screen = pygame.display.set_mode((1200, 675))
         pygame.display.set_caption("Alien Invasion")
+        self.bg_color = (230, 230, 230)
 
     def run_game(self):
         """Start the main loop for the game"""
@@ -19,6 +20,7 @@ class AlienInvasion:
                 if event.type == pygame.QUIT:
                     sys.exit()
             pygame.display.flip()
+            self.screen.fill(self.bg_color)
 
 
 if __name__ == '__main__':
