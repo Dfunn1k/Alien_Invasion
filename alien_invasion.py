2a517bc67f8677714e62f67731765d8790994812
Dfunn1k <danychavezdev@gmail.com>
2023-10-03 17:44:09 -0500

    feat: added new instance for class Settings

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,5 +1,6 @@
 import sys
 import pygame
+from settings import Settings
 
 
 class AlienInvasion:
@@ -8,10 +9,11 @@ class AlienInvasion:
     def __init__(self):
         """Initialize the game and create resource"""
         pygame.init()
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height))
 
-        self.screen = pygame.display.set_mode((1200, 675))
         pygame.display.set_caption("Alien Invasion")
-        self.bg_color = (230, 230, 230)
 
     def run_game(self):
         """Start the main loop for the game"""
@@ -20,7 +22,8 @@ class AlienInvasion:
                 if event.type == pygame.QUIT:
                     sys.exit()
             pygame.display.flip()
-            self.screen.fill(self.bg_color)
+            # redraw the screen on each step throught the loop
+            self.screen.fill(self.settings.bg_color)
 
 
 if __name__ == '__main__':
