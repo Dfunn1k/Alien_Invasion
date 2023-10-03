8ba0c0083e6236694a368049af7e921fc27d70e0
Dfunn1k <danychavezdev@gmail.com>
2023-10-03 17:43:33 -0500

    feat: added new module containing a class for settings

---
+++ settings.py
@@ -0,0 +1,8 @@
+class Settings:
+    """Class for save all game settings"""
+
+    def __init__(self):
+        """Initialize the setting of the game"""
+        self.screen_width = 1200
+        self.screen_height = 675
+        self.bg_color = (230, 230, 230)
